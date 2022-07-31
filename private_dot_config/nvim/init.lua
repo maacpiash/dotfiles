7001daf8f90f51832f3ba9a6d11fc1f7b758a4fa
Ahad Chowdhury <ahad@maacpiash.com>
2022-07-31 16:10:31 +1000

    🚚 Change path to OmniSharp
    The new path is in ~/Development/Tools with version specified.

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -54,6 +54,6 @@ require('lualine').setup {
 }
 
 require('lspconfig').omnisharp.setup{
-  cmd = { 'dotnet', '/home/ahad/omnisharp-roslyn/OmniSharp.dll' }
+  cmd = { 'dotnet', '/home/ahad/Development/Tools/omnisharp-roslyn-v1.39.1/OmniSharp.dll' }
 }
 -- For JS/TS/JSX/TSX, JSON, and Python, I am using CoC.nvim.
