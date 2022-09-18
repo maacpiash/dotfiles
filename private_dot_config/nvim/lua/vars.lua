5cfe89804748e78c0788f51fe7944efcb87d087f
Ahad Chowdhury <ahad@maacpiash.com>
2022-09-19 02:42:07 +1000

    🚨 Fix config to remove warnings
    No more warning regarding Perl, Python, and Ruby providers.

--- private_dot_config/nvim/lua/vars.lua
+++ private_dot_config/nvim/lua/vars.lua
@@ -4,6 +4,10 @@ g.background = 'dark'
 g.mapleader = ','
 g.localleader = '\\'
 g.OmniSharp_server_use_net6 = 1
+g.loaded_perl_provider = 0
+g.loaded_python_provider = 0
+g.loaded_ruby_provider = 0
+g.python3_host_prog = vim.fn.system('which python3'):gsub('\n+', '')
 vim.cmd[[colorscheme darkplus]]
 
 g.EditorConfig_exec_path = '/home/ahad/.dotnet/tools/editorconfig'
