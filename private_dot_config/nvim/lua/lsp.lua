d9b663863daff7359b2079aa8efa5e1a56f3d70b
Ahad Chowdhury <ahad@maacpiash.com>
2022-09-19 02:43:55 +1000

    ♻️ Simplify LSP configuration
    Default values were removed, variables were used for repetitive values.

--- private_dot_config/nvim/lua/lsp.lua
+++ private_dot_config/nvim/lua/lsp.lua
@@ -1,75 +1,49 @@
--- OmniSharp --
-
-local omnisharp_config = {
-  cmd = { 'dotnet', '/home/ahad/Development/Tools/omnisharp-roslyn-v1.39.1/OmniSharp.dll' },
-  handlers = {
-    ['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-    ['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
-  }
-}
-
--- Python --
-
-local python_config = {
-  cmd = {'pyright-langserver', '--stdio'}   -- default
-}
-
--- TypeScript --
-
-local typescript_config = {
-  handlers = {
-    ['textDocument/hover'] = vim.lsp.with( vim.lsp.handlers.hover, { border = 'rounded' }),
-    ['textDocument/signatureHelp'] = vim.lsp.with( vim.lsp.handlers.signature_help, { border = 'rounded' }),
-  },
-  cmd = { 'typescript-language-server', '--stdio' }, -- default
-  filetypes = {
-    'javascript',
-    'javascriptreact',
-    'javascript.jsx',
-    'typescript',
-    'typescriptreact',
-    'typescript.tsx'
-  },
-  init_options = { hostInfo = 'neovim' },
-  root_dir = require('lspconfig.util').root_pattern('package.json', 'tsconfig.json', '.git'),
-}
-
--- Final --
-
 local lspconfig = require('lspconfig')
 
 local on_attach = function(client, bufnr)
-  local buf_set_keymap = vim.keymap.set
+  local set = vim.keymap.set
 
   local opts = { noremap = true, silent = true, buffer = bufnr }
   
   if client.name == 'omnisharp' then
-    buf_set_keymap('n', 'gd', '<Cmd>OmniSharpGotoDefinition<CR>', opts)
+    set('n', 'gd', '<Cmd>OmniSharpGotoDefinition<CR>', opts)
   else
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    set('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
   end
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  set('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  set('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  set('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  set('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  set('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  set('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  set('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  set('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  set('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  set('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  set('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  set('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
+local rounded_border_handlers = {
+  ['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' }),
+  ['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' }),
+}
+
 local lang_servers = {
-  omnisharp = omnisharp_config,
-  tsserver = typescript_config,
-  pyright = python_config,
+  omnisharp = {
+    cmd = { 'dotnet', '/home/ahad/Development/Tools/omnisharp-roslyn-v1.39.1/OmniSharp.dll' },
+    handlers = rounded_border_handlers,
+  },
+  tsserver = {
+    handlers = rounded_border_handlers,
+  },
+  pyright = {
+    handlers = rounded_border_handlers,
+  },
 }
 
 for name, config in pairs(lang_servers) do
