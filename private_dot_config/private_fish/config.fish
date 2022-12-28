00213d01ce13903ca3f984bc4e4fd109ab512ab6
Ahad Chowdhury <ahad@maacpiash.com>
2022-12-28 18:50:20 +1100

    ⚰️ Remove commands for OMP and Anaconda
    Not using OMP and Anaconda anymore. Using Starship and Venv instead.

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,9 +1,4 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-oh-my-posh init fish --config ~/.omp.json | source
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
+starship init fish | source
