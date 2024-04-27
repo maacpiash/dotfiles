d5d77bb5d54503afdb80298a3ab11456fba59e59
Ahad Chowdhury <ahad@maacpiash.com>
2024-04-27 20:09:07 +1000

    🚧 Add LSP support via Mason and LSPConfig

---
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,40 @@
+return {
+   {
+      "williamboman/mason.nvim",
+      config = function()
+         require("mason").setup()
+      end
+   },
+   {
+      "williamboman/mason-lspconfig.nvim",
+      config = function()
+         require("mason-lspconfig").setup({
+            ensure_installed = {
+               "omnisharp",
+               "tsserver",
+               "pyright",
+               "gopls",
+               "gleam",
+               "html",
+               "astro",
+               "emmet_ls",
+               "cssls",
+               "eslint",
+               "clangd",
+               "lua_ls",
+               "rust_analyzer",
+               "svelte",
+            },
+         })
+      end
+   },
+   {
+      "neovim/nvim-lspconfig",
+      config = function()
+         local lspconfig = require("lspconfig")
+         lspconfig.lua_ls.setup({})
+         vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true })
+         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { noremap = true, silent = true })
+      end
+   }
+}
