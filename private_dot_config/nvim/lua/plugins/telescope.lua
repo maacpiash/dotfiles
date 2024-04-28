79fb34cd87773f82f69b3bf7dfafb0dda7f5a291
Ahad Chowdhury <ahad@maacpiash.com>
2024-04-28 23:55:10 +1000

    ➕ Add `telescope-ui-select.nvim`
    For prettier lists with inputs

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -8,5 +8,18 @@ return {
          vim.keymap.set("n", "<C-p>", builtin.find_files, { noremap = true, silent = true })
          vim.keymap.set("n", "<leader>fg", builtin.live_grep, { noremap = true, silent = true })
       end
+   },
+   {
+      "nvim-telescope/telescope-ui-select.nvim",
+      config = function()
+         require("telescope").setup {
+            extensions = {
+               ["ui-select"] = {
+                  require("telescope.themes").get_dropdown({})
+               }
+            }
+         }
+         require("telescope").load_extension("ui-select")
+      end
    }
 }
