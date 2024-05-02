72dc33af60e4b73fca8b9e63aa8081980690f7af
Ahad Chowdhury <ahad@maacpiash.com>
2024-05-02 14:36:32 +1000

    🐛 Fix ESLint issue with an extra plugin
    ESLint can now be used for CA, diagnostics, and formatting.

--- private_dot_config/nvim/lua/plugins/none-ls.lua
+++ private_dot_config/nvim/lua/plugins/none-ls.lua
@@ -1,12 +1,17 @@
 return {
    {
       "nvimtools/none-ls.nvim",
+      dependencies = {
+         "nvimtools/none-ls-extras.nvim",
+      },
       config = function()
          local null_ls = require("null-ls")
          null_ls.setup({
             sources = {
                null_ls.builtins.formatting.stylua,
-               null_ls.builtins.diagnostics.eslint_d,
+               require("none-ls.code_actions.eslint_d"),
+               require("none-ls.diagnostics.eslint_d"),
+               require("none-ls.formatting.eslint_d"),
                null_ls.builtins.formatting.prettier,
                null_ls.builtins.formatting.black,
             },
